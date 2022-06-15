9e57720f2e8cae264fee75bbeca1a4b6bb9084ac
JavierOramas <javiale2000@gmail.com>
2022-06-15 10:45:55 -0400

    rendering correctly accents

--- utils.py
+++ utils.py
@@ -3,3 +3,8 @@ import math
 def round_half_up(n, decimals=0):
     multiplier = 10 ** decimals
     return math.floor(int(n)*multiplier + 0.5) / multiplier
+
+def get_rbt_coordinator(db):
+    coordinator = db.users.find_one({'ProviderId': 1382528})
+    print(coordinator)
+    return coordinator['name']
