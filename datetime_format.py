7967056b464c69123f2bb3dcffd73a6f8ca9cb4c
Joramas <javiale2000@gmai.com>
2024-02-21 14:23:28 -0500

    fix date wiyh dash

--- datetime_format.py
+++ datetime_format.py
@@ -1,11 +1,18 @@
 import datetime
 def format(date):
+    
+    if "-" in date:
+        date = date.replace("-", "/")
+
     try:
         return datetime.datetime.strptime(date, '%m/%d/%Y %H:%M').strftime('%m/%d/%Y %H:%M')
     except:
         return datetime.datetime.strptime(date, '%m/%d/%Y %H:%M').strftime('%d/%m/%Y %H:%M')
 
 def get_date(date):
+    if "-" in date:
+        date = date.replace("-", "/")
+
     if date[0] == ' ':
         date = date[1:]
     if date[-1] == ' ':
